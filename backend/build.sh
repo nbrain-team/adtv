c6f975a65a3b614a5d326422eb55224d875205c2
Danny DeMichele <danny@nbrain.com>
2025-06-23 16:12:01 -0700

    fix: Remove system-level dependencies from build script to fix deployment

--- backend/build.sh
+++ backend/build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-# --- Install System Dependencies ---
-apt-get update && apt-get install -y ffmpeg
-
 # --- Install Python Dependencies ---
 pip install -r requirements.txt
 
