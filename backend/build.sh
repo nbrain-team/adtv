6a27203815db550045a8f055ae33fdee0ee49e79
Danny DeMichele <danny@nbrain.com>
2025-06-25 16:17:07 -0700

    revert: Remove incorrect Chrome installation from build script

--- backend/build.sh
+++ backend/build.sh
@@ -2,15 +2,6 @@
 # exit on error
 set -o errexit
 
-# --- Install Google Chrome for Selenium ---
-echo "Installing Google Chrome..."
-apt-get update
-apt-get install -y wget
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
-echo "Google Chrome installation complete."
-
 # --- Install Python Dependencies ---
 pip install -r requirements.txt
 
